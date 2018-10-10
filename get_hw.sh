bced4200c2387ba22dbf0342765dcd6ebe819a49
jmausolf <jgm346@gmail.com>
2018-10-10 12:01:27 -0500

    fixing another user typo

--- get_hw.sh
+++ get_hw.sh
@@ -10,7 +10,7 @@
 
 
 #Define List of User Names
-users='dhruvalb KenChenCompEcon keertanavc jtschoi yundai424 RSFlores jsgenan NetaGee fulinguo cosettelh hanjiaxu ellenhsieh ShuyanHuang nerdizzyz ruixili ShanglunLi liu431 SixueLiuMACSS hannamn madelaida smiklin policyglot SiyuanPengMike boyangqu Haowen Shang tonofshell bhargavvader sunying2018 sanittawan josetan delores9584 nt546 di-Tong zeyuxu1997 yalingtsui dongchengecon Bobicheng-Zhang TianxinZheng AZorroMedina'
+users='dhruvalb KenChenCompEcon keertanavc jtschoi yundai424 RSFlores jsgenan NetaGee fulinguo cosettelh hanjiaxu ellenhsieh ShuyanHuang nerdizzyz ruixili ShanglunLi liu431 SixueLiuMACSS hannamn madelaida smiklin policyglot SiyuanPengMike boyangqu HaowenShang tonofshell bhargavvader sunying2018 sanittawan josetan delores9584 nt546 di-Tong zeyuxu1997 yalingtsui dongchengecon Bobicheng-Zhang TianxinZheng AZorroMedina'
 
 #Specify Fork
 fork="DescrStats"
