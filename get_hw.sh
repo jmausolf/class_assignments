580b3c6ff7e1855f26e9c8bdc36d9116b4bef282
jmausolf <jgm346@gmail.com>
2018-10-10 11:56:03 -0500

    fixing github user typo

--- get_hw.sh
+++ get_hw.sh
@@ -10,7 +10,7 @@
 
 
 #Define List of User Names
-users='dhruvalb KenChenCompEcon keertanavc jtschoi yundai424 RSFlores jsgenan NetaGee fulinguo cosettelh hanjiaxu ellenhsieh ShuyanHuang nerdizzyz ruixili ShanglunLi liu431 SixueLiuMACSS hannamn madelaida smiklin policyglot SiyuanPengMike boyangqu Haowen Shang tonofshell bhargavvader sunying2018 sanittawan josetan delores9584 nt546 di-Tong zeyuxu1997 yalingtsui dongchengecon Bobicheng-Zhang TianxinZheng AZorroMedin'
+users='dhruvalb KenChenCompEcon keertanavc jtschoi yundai424 RSFlores jsgenan NetaGee fulinguo cosettelh hanjiaxu ellenhsieh ShuyanHuang nerdizzyz ruixili ShanglunLi liu431 SixueLiuMACSS hannamn madelaida smiklin policyglot SiyuanPengMike boyangqu Haowen Shang tonofshell bhargavvader sunying2018 sanittawan josetan delores9584 nt546 di-Tong zeyuxu1997 yalingtsui dongchengecon Bobicheng-Zhang TianxinZheng AZorroMedina'
 
 #Specify Fork
 fork="DescrStats"
